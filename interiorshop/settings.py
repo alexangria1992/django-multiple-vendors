3b8cd4324c863503b482ae519067fa404b151aa2
alexangria1992 <alex040892@gmail.com>
2021-01-21 02:23:08 +1300

    Vendor App, Vendor Model, View, Template and Form for Sign Up, Simple Area For Testing

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.core'
+    'apps.core',
+    'apps.vendor'
 ]
 
 MIDDLEWARE = [
