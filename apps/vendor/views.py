1e574c2596a6989044757ce9ec25c6ad68f2c229
alexangria1992 <alex040892@gmail.com>
2021-01-21 22:34:11 +1300

    Product App, List of Products

--- apps/vendor/views.py
+++ apps/vendor/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Vendor
+from apps.product.models import Product
+
 # Create your views here.
 def become_vendor(request):
     if request.method == 'POST':
@@ -21,5 +23,6 @@ def become_vendor(request):
 @login_required
 def vendor_admin(request):
     vendor = request.user.vendor
+    products = vendor.products.all()
     
-    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core',
-    'apps.vendor'
+    'apps.vendor',
+    'apps.product'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
+MEDIA_URL = '/media/'
+MEDA_ROOT = BASE_DIR / 'media/'
+
