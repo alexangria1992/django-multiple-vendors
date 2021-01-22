8f1dd3c75b410a06c8decbade62849436bf47ed6
alexangria1992 <alex040892@gmail.com>
2021-01-22 13:14:38 +1300

    Fixed Bugs

--- apps/product/models.py
+++ apps/product/models.py
@@ -33,15 +33,16 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def get_thumbanil(self):
+
+    def get_thumbnail(self):
         if self.thumbnail:
-            return self.thumbnail.url 
+            return self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
 
-                return self.thumbnail.url 
+                return self.thumbnail.url
             else:
                 return 'https://via.placeholder.com/240x180.jpg'
 
@@ -52,6 +53,7 @@ class Product(models.Model):
 
         thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
+
         thumbnail = File(thumb_io, name=image.name)
 
-        return thumbnail
+        return thumbnail 
